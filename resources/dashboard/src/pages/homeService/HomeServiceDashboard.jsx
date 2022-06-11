69df523a48202067aa6f20edbbb97acb391ba461
Dipankar Saikia <er.dipankarsaikia@gmail.com>
2022-06-11 23:48:03 +0530

    Added categories and services links in MasterAdminDashboard, added logo to the navbars

--- resources/dashboard/src/pages/homeService/HomeServiceDashboard.jsx
+++ resources/dashboard/src/pages/homeService/HomeServiceDashboard.jsx
@@ -2,6 +2,7 @@ import {Link} from 'react-router-dom'
 import { Outlet } from "react-router-dom"
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
+import logo from '../../logo.jpeg';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -21,7 +22,9 @@ function HomeServiceDashboard() {
       
       <Navbar key={expand} bg={theme} variant={theme} expand={expand} className="mb-3">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+          <Navbar.Brand href="#/homeService">
+            <img src={logo} alt="DCleans.com" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-${expand}`}
@@ -30,12 +33,12 @@ function HomeServiceDashboard() {
           >
             <Offcanvas.Header closeButton>
               <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                Offcanvas
+                <img src={logo} alt="DCleans.com" />
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#/">Home</Nav.Link>
                 <Nav.Link href="#action2">Link</Nav.Link>
                 <NavDropdown
                   title="Dropdown"
--- resources/dashboard/src/pages/master/MasterAdminDashboard.jsx
+++ resources/dashboard/src/pages/master/MasterAdminDashboard.jsx
@@ -2,18 +2,12 @@ import {Link} from 'react-router-dom'
 import { Outlet } from "react-router-dom"
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import Navbar from 'react-bootstrap/Navbar';
-// import Nav from 'react-bootstrap/Nav';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-// import Container from 'react-bootstrap/Container';
+import logo from '../../logo.jpeg';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
 
 function MasterAdminDashboard() {
   
@@ -23,32 +17,7 @@ function MasterAdminDashboard() {
   return (
     <>
 
-      {/* <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-        <Navbar.Brand href="#/master">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        </Container>
-      </Navbar> 
-      
+      {/* 
       <header className='header'>
         <li>
           <Link to='/master/shops'>
@@ -75,7 +44,9 @@ function MasterAdminDashboard() {
 
       <Navbar key={expand} bg={theme} variant={theme} expand={expand} className="mb-3">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+          <Navbar.Brand href="/master">
+            <img src={logo} alt="DCleans.com" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-${expand}`}
@@ -84,36 +55,39 @@ function MasterAdminDashboard() {
           >
             <Offcanvas.Header closeButton>
               <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                Offcanvas
+                <img src={logo} alt="DCleans.com" />
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown
-                  title="Dropdown"
-                  id={`offcanvasNavbarDropdown-expand-${expand}`}
-                >
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
+                <Nav.Link href="#/">Home</Nav.Link>
+
+                <NavDropdown title="Shops" id={`offcanvasNavbarDropdown-expand-${expand}`}>
+                  <NavDropdown.Item href="#/master/shops">Shops</NavDropdown.Item>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#/master/shops/1">Shop</NavDropdown.Item>
+                  <NavDropdown.Item href="#/master/shops/create">Add Shop</NavDropdown.Item>
+                  <NavDropdown.Item href="#/master/shops/edit/1">Edit Shop Shop</NavDropdown.Item>
                 </NavDropdown>
+
+                <NavDropdown title="Categories" id={`offcanvasNavbarDropdown-expand-${expand}`}>
+                  <NavDropdown.Item href="#/master/categories">Categories</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#/master/categories/1">Category</NavDropdown.Item>
+                  <NavDropdown.Item href="#/master/categories/create">Add Category</NavDropdown.Item>
+                  <NavDropdown.Item href="#/master/categories/edit/1">Edit Category</NavDropdown.Item>
+                </NavDropdown>
+
+                <NavDropdown title="Services" id={`offcanvasNavbarDropdown-expand-${expand}`}>
+                  <NavDropdown.Item href="#/master/services">Services</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#/master/services/1">Service</NavDropdown.Item>
+                  <NavDropdown.Item href="#/master/services/create">Add Service</NavDropdown.Item>
+                  <NavDropdown.Item href="#/master/services/edit/1">Edit Shop Service</NavDropdown.Item>
+                </NavDropdown>
+
               </Nav>
-              <Form className="d-flex">
-                <FormControl
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
+
             </Offcanvas.Body>
           </Navbar.Offcanvas>
         </Container>
