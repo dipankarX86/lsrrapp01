b087a4bc3e37999246f6ccd69d670321e4c96174
Dipankar Saikia <er.dipankarsaikia@gmail.com>
2022-06-28 11:23:12 +0530

    Address seeding done again

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'line1' => $this->faker->streetAddress(),
+            'line2' =>  $this->faker->streetAddress(),
+            'city' => '1',
+            'state' => '1',
+            'country' => '1',
+            'postal_code' =>  $this->faker->postcode(),
+        ];
+    }
+}
